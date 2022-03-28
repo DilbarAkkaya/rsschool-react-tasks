aca865767f776846e82aa374e32fe5881c79a718
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-03-28 22:42:59 +0300

    feat: add test for localstorage

---
+++ components/src/component/Search/LocalStorage.test.tsx
@@ -0,0 +1,37 @@
+import { saveToStorage } from './storage';
+
+interface IStorage {
+  [key: string] : string
+}
+const fakeLocalStorage = (function () {
+  let store: IStorage = {};
+
+  return {
+    getItem: function (key: string) {
+      return store[key] || null;
+    },
+    setItem: function (key: string, value: string) {
+      store[key] = value.toString();
+    },
+    removeItem: function (key: string) {
+      delete store[key];
+    },
+    clear: function () {
+      store = {};
+    }
+  };
+})();
+
+describe('storage', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'localStorage', {
+      value: fakeLocalStorage,
+    });
+  });
+
+  it('saves the key to the storage', () => {
+    saveToStorage('fake-value');
+
+    expect(window.localStorage.getItem('the-key')).toEqual('fake-value');
+  });
+});
