a701752e0fb8f27c0e10465a7ee92d0a3ccff185
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-03-28 21:57:16 +0300

    feat: add test files

---
+++ components/src/pages/AboutUs.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import { BrowserRouter } from 'react-router-dom';
+
+test('AboutUs', () => {
+  render(<BrowserRouter><App /></BrowserRouter>);
+  const linkElement = screen.getByText(/About Us/i);
+  expect(linkElement).toBeInTheDocument();
+});
