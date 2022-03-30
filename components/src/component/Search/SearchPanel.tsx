2d5a116dae81335ff32357085843131baedba778
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-03-31 02:02:34 +0300

    refactor: change according to the documentation

--- components/src/component/Search/SearchPanel.tsx
+++ components/src/component/Search/SearchPanel.tsx
@@ -1,17 +1,26 @@
 import React, { ChangeEvent } from 'react';
 import './search.css';
 
-class SearchPanel extends React.Component {
-  state = {
-    value: '',
-  };
+type MyProps = { value?: string };
+type MyState = { value: string };
+
+class SearchPanel extends React.Component<MyProps, MyState> {
+  constructor(props: MyProps) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+  }
 
   handleChange(event: ChangeEvent<HTMLInputElement>) {
     this.setState({ value: event.target.value });
   }
 
   componentDidMount() {
-    this.setState({ value: localStorage.getItem('searchItem') });
+    this.setState({ value: localStorage.getItem('searchItem') as string });
   }
 
   componentWillUnmount() {
