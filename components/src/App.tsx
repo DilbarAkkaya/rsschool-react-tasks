ea3401e848f04871bc499efe9d57de578add0990
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-03-30 23:36:50 +0300

    refactor: move path variables to constant

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -6,14 +6,18 @@ import AboutUs from './pages/AboutUs';
 import NotFound from './pages/404';
 
 function App() {
+  const PATH_MAIN = '/',
+    PATH_ABOUT = '/aboutus',
+    PATH_404 = '404',
+    PATH_OTHER = '*';
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path={PATH_MAIN} element={<Main />} />
+        <Route path={PATH_ABOUT} element={<AboutUs />} />
+        <Route path={PATH_404} element={<NotFound />} />
+        <Route path={PATH_OTHER} element={<NotFound />} />
       </Routes>
     </div>
   );
