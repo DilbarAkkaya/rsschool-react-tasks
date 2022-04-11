fbf8f4ba5ea7bbaf09699d36768f7cd9d8c99dfb
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-04-11 23:50:26 +0300

    feat: modified errormessage tsx, add error.css

---
+++ components/src/component/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import img from './error.gif';
+import './modal';
+
+const ErrorMessage = () => {
+  return (
+    <img src={img} alt=""/>
+  )
+}
+
+export default ErrorMessage;
