a3ae17cb21ca9a57b00c0b39df6bba326a092fe2
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-04-13 01:10:00 +0300

    feat: add test and mock fetch

--- components/src/pages/Main.test.tsx
+++ components/src/pages/Main.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+import Main from '../pages/Main'
 
 test('Main page', () => {
   render(
@@ -11,4 +14,24 @@ test('Main page', () => {
     
   const linkElement = screen.getByText(/Main page/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+const hits = [
+  {
+    objectID: "1", title: "Angular"
+  },
+  {
+    objectID: "2", title: "React"
+  }
+];
+describe("App", ()=> {
+  it("fetch", async ()=>{
+    mockedAxios.get.mockImplementationOnce(() => Promise.resolve({data: {hits}}));
+    const {getByRole, findAllByRole} = render (<Main/>);
+    userEvent.click(getByRole("textbox"));
+    const items = await findAllByRole("listitem");
+    expect(items).toHaveLength(6);
+  });
 });
