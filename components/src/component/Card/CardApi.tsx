0172049379f101d82fe0793e279378bd61c15a56
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-04-16 22:01:20 +0300

    refactor: delete comments

--- components/src/component/Card/CardApi.tsx
+++ components/src/component/Card/CardApi.tsx
@@ -32,37 +32,6 @@ const CardApi =(props: cardApiProps) => {
       </div>
     );
   }
-/* 
-class CardApi extends React.Component<cardApiProps> {
-  constructor(props: cardApiProps) {
-    super(props);
-    this.handleCard = this.handleCard.bind(this);
-  }
-  handleCard() {
-    this.props.handleClick(this.props.id);
-  }
-  render() {
-    return (
-      <div className="card" onClick={this.handleCard}>
-        <h2 className="card-title">{this.props.name}</h2>
-        <img src={this.props.image} alt="toy" className="card-img" />
-        <div className="card-descr">
-          <p>
-            Status:
-            <span>{this.props.status}</span>
-          </p>
-          <p>
-            Species:
-            <span>{this.props.species}</span>
-          </p>
-          <p>
-            Gender:
-            <span>{this.props.gender}</span>
-          </p>
-        </div>
-      </div>
-    );
-  }
-} */
+
 
 export default CardApi;
--- components/src/component/Search/SearchPanel.tsx
+++ components/src/component/Search/SearchPanel.tsx
@@ -15,7 +15,6 @@ const SearchPanel  = (props: MyProps) => {
   useEffect(()=>{
     
     return (()=> {
-      //console.log(value)
       localStorage.setItem('searchItem', valueRef.current);
     }
   )},[]);
@@ -40,51 +39,3 @@ const SearchPanel  = (props: MyProps) => {
   }
 
 export default SearchPanel;
-
-/* 
-class SearchPanel extends React.Component<MyProps, MyState> {
-  constructor(props: MyProps) {
-    super(props);
-    this.state = {
-      value: '',
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.componentDidMount = this.componentDidMount.bind(this);
-    this.componentWillUnmount = this.componentWillUnmount.bind(this);
-    this.keyPressHandler = this.keyPressHandler.bind(this);
-  }
-
-  handleChange(event: ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.target.value });
-    localStorage.setItem('searchItem', event.target.value);
-  }
-
-  componentDidMount() {
-    this.setState({ value: localStorage.getItem('searchItem') as string });
-  }
-
-  componentWillUnmount() {
-    localStorage.setItem('searchItem', this.state.value);
-  }
-
-  keyPressHandler(event: React.KeyboardEvent) {
-    if (event.key === 'Enter') {
-      this.props.onSearchData(this.state.value);
-    }
-  }
-
-  render() {
-    return (
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search..."
-        value={this.state.value}
-        onChange={this.handleChange}
-        onKeyPress={this.keyPressHandler}
-      />
-    );
-  }
-}
-
-export default SearchPanel; */
