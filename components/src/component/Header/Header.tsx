a8cd75af5560e39f792d64435dc32c31c40fbd34
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-04-20 01:04:54 +0300

    feat: change header component

--- components/src/component/Header/Header.tsx
+++ components/src/component/Header/Header.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { PATH } from '../../constants/constants';
 import './header.css';
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <NavLink to={PATH.MAIN}>Main</NavLink>
-        <NavLink to={PATH.FORM}>Form</NavLink>
-        <NavLink to={PATH.ABOUT}>About Us</NavLink>
-        <NavLink to={PATH.OTHER}>404</NavLink>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className="header">
+      <NavLink to={PATH.MAIN}>Main</NavLink>
+      <NavLink to={PATH.FORM}>Form</NavLink>
+      <NavLink to={PATH.ABOUT}>About Us</NavLink>
+      <NavLink to={PATH.OTHER}>404</NavLink>
+    </header>
+  );
+};
 
 export default Header;
